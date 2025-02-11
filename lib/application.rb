8c6af0779c25cfacefd4e4cc4ac482366daed1c1
Ken Sullivan <ken.sullivan@magentus.com>
2025-02-11 12:05:45 +1000

    Added logic to handle input

--- lib/application.rb
+++ lib/application.rb
@@ -4,13 +4,50 @@ class Application
   def initialize(stdin, stdout)
     @stdin = stdin
     @stdout = stdout
+    @playing = true
   end
 
   def start
-    puts 'Success'
+    puts 'Welcome to Toy Robot Simulator!'
+
+    while @playing
+      puts 'Enter command'
+
+      handle_command(stdin.gets.chomp.split)
+    end
+
+    puts 'Thank you for playing!'
   end
 
   private
+
+  def handle_command(commands)
+    case commands[0].upcase
+    when 'PLACE'
+      place(commands[1..])
+    when 'MOVE', 'LEFT', 'RIGHT', 'HELP', 'REPORT'
+      not_implemented
+    when 'QUIT', 'Q'
+      terminate
+    else puts 'Invalid command'
+    end
+  end
+
+  def terminate
+    @playing = false
+  end
+
+  def not_implemented
+    puts 'Method not implemented'
+  end
+
+  def puts(message)
+    stdout.puts message
+  end
+
+  def place(args)
+    puts "PLACE args: #{args}"
+  end
 
   attr_reader :stdin, :stdout
 
