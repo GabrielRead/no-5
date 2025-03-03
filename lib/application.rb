605aaefd39f54566f214208a6d0901b0ad7a43d2
Nelson Gregory <nelson.gregory@magentus.com>
2025-03-04 09:41:18 +1000

    exercise

--- lib/application.rb
+++ lib/application.rb
@@ -1,15 +1,40 @@
+require 'debug'
 # frozen_string_literal: true
 
 class Application
   def initialize(stdin, stdout)
     @stdin = stdin
     @stdout = stdout
+    @bounds_x = 5
+    @bounds_y = 5
+    @position = nil
   end
 
   def start
-    while (input = stdin.gets)
-      put input
+    while (input = stdin.gets.chomp)
+      command = parse_command(input)
+      execute(command)
     end
+  end
+
+  def parse_command(input)
+    if input.start_with?('PLACE')
+      @position
+    elsif input == 'REPORT'
+      @position
+    elsif input == 'EXIT'
+      exit 0
+    elsif input == 'Hello'
+      puts 'hello'
+    else
+      # binding.break
+      puts format('Unrecognized command: %<input>s', input: input)
+    end
+  end
+
+  def execute(command)
+    # See if the new position is valid, save it if it is or show an error
+    nil if command.nil?
   end
 
   private
---
+++ lib/robot.rb
@@ -0,0 +1,5 @@
+class Robot
+  def hello
+    puts 'hello'
+  end
+end
